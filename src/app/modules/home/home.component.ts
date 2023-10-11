7ab338076e5a28bafc8dd9e4c23fc8da92fc6efc
ANH Nguyen Tri Hoang (VTI.D8) <anh.nguyentrihoang@vti.com.vn>
2023-10-11 17:32:46 +0700

    add new card in home

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -24,28 +24,37 @@ export class HomeComponent implements OnInit {
       img: 'https://3kshop.vn/wp-content/uploads/fly-images/37385/230d75d2a50360eebf9a76a040bffb9f-450x450-c.jpeg',
       name: 'Klipsch The One II',
       cost: 4800000,
+      discount: 25,
       isFirst: true,
     },
     {
       img: 'https://3kshop.vn/wp-content/uploads/fly-images/37385/230d75d2a50360eebf9a76a040bffb9f-450x450-c.jpeg',
       name: 'Noble Stage 3',
       cost: 6400000,
+      discount: null,
     },
     {
       img: 'https://3kshop.vn/wp-content/uploads/fly-images/50621/noble-stage-3-1-450x450-c.jpeg',
       name: 'Yamaha YH-G01',
       cost: 4990000,
+      discount: null,
     },
     {
       img: 'https://3kshop.vn/wp-content/uploads/fly-images/37385/230d75d2a50360eebf9a76a040bffb9f-450x450-c.jpeg',
       name: 'Elgato Cam Link 4K ',
       cost: 4580000,
+      discount: null,
     },
     {
       img: 'https://3kshop.vn/wp-content/uploads/fly-images/50542/yamaha-yg-01-2-450x450-c.jpeg',
       name: 'Elgato Cam Link 4K',
       cost: 2895000,
+      discount: null,
     },
   ];
+
+  getDiscountPrice(cost: number, discount: number): number {
+    return cost * (1 - discount / 100);
+  }
   ngOnInit() {}
 }
