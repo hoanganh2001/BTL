6366e45061ffd2db3ada49eac621923f2cebbe6e
ANH Nguyen Tri Hoang (VTI.D8) <anh.nguyentrihoang@vti.com.vn>
2023-10-17 15:12:30 +0700

    add common css animation

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, ReactiveFormsModule],
-  exports: [CommonModule, FormsModule, ReactiveFormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, RouterModule],
+  exports: [CommonModule, FormsModule, ReactiveFormsModule, RouterModule],
   declarations: [],
 })
 export class SharedModule {}
